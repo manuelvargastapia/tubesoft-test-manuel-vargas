ea63d561034c2d8d9dbdc09f3232300d16a25428
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-16 19:28:50 -0400

    split server and app and create health check endpoint

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+// Endpoint to verify the application is up and ready.
+//
+// It can be used to customize some alert system to get noitified when
+// the API is down. Or trigger a restart in the context of container
+// orchestration when the API has become unresponsive.
+app.get("/health_check", (_, res) => {
+  res.status(200).send();
+});
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,9 @@
+// We separate the app from the server for testing purposes:
+// we can run the app with `node server.js` and also import
+// app.js for testing
+const app = require("./app");
+
+app.listen(3000, () => {
+  // TODO: replace by `debug`
+  console.log("Listening on port 3000");
+});
