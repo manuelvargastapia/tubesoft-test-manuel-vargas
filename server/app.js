127a54bf7e58cba3156237ff6b658ff0d9783c09
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-17 11:44:30 -0400

    add /register_time endpoint and validate body

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const Joi = require('joi');
 
 const app = express();
+
+const validator = require('express-joi-validation').createValidator();
+
+const querySchema = Joi.object({
+  registeredTime: Joi.number().integer().min(0).max(2147483647).required(),
+});
+
+// Parse application/json
+app.use(express.json());
 
 // Endpoint to verify the application is up and ready.
 //
@@ -8,6 +18,11 @@ const app = express();
 // the API is down. Or trigger a restart in the context of container
 // orchestration when the API has become unresponsive.
 app.get('/health_check', (_, res) => {
+  res.status(200).send();
+});
+
+// Endpoint to register the time sended by client in miliseconds
+app.post('/register_time', validator.body(querySchema), (_, res) => {
   res.status(200).send();
 });
 
