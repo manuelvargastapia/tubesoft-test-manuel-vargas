2a19c86f3e8ba8fdab1a3b66615d10cb507104bb
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-17 11:44:01 -0400

    test-drive /register_time endpoint

--- server/app.test.js
+++ server/app.test.js
@@ -7,3 +7,45 @@ describe('GET /health_check', () => {
     return request(app).get('/health_check').expect(200);
   });
 });
+
+describe('POST /register_time', () => {
+  test('It should return OK 200 for valid data', () => {
+    return request(app)
+      .post('/register_time')
+      .send({ registeredTime: 90000 })
+      .expect(200);
+  });
+
+  test('It should return BAD REQUEST 400 when data is missing', (done) => {
+    const testCases = [{ registeredTime: null }, null];
+    for (const invalidData in testCases) {
+      request(app)
+        .post('/register_time')
+        .send(invalidData)
+        .then((response) => {
+          expect(response.statusCode).toBe(400);
+          done();
+        });
+    }
+  });
+
+  test('It should return BAD REQUEST 400 when registeredTime is invalid', (done) => {
+    // It shouldn't be negative, decimal or > 2147483647 (max DB storage for integers)
+    const testCases = [
+      { registeredTime: -1000 },
+      { registeredTime: 123.0 },
+      { registeredTime: 456.789 },
+      { registeredTime: 2147483648 },
+    ];
+
+    for (const invalidData in testCases) {
+      request(app)
+        .post('/register_time')
+        .send(invalidData)
+        .then((response) => {
+          expect(response.statusCode).toBe(400);
+          done();
+        });
+    }
+  });
+});
