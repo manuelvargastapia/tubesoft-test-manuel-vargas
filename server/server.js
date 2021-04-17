ee039a7aceb1260f39cf27510ec203fd3e7d0cc9
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-17 14:29:57 -0400

    autheticate db connection

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@
 // we can run the app with `node server.js` and also import
 // app.js for testing
 const app = require('./app');
+const { sequelize } = require('./models');
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
   // TODO: replace by `debug`
   console.log('Listening on port 3000');
+  await sequelize.authenticate();
+  console.log('Connected to Postgres!');
 });
