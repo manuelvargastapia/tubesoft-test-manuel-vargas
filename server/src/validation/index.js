fd081f1c4ca951262de36b60884f72ef0e4c9a41
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-18 13:14:28 -0400

    add comments and improve nomenclature

--- server/src/validation/index.js
+++ server/src/validation/index.js
@@ -1,10 +1,15 @@
+// We use Joi to validate the requests before handling them.
+// Any error is forwarded to the `errorHandler` controller.
+
 const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({
   passError: true,
 });
 
-const querySchema = Joi.object({
+// Define a schema to accept any integer number between 0 and 2147483647
+// (tha maximum integer value accepted by database)
+const bodySchema = Joi.object({
   registeredTime: Joi.number().integer().min(0).max(2147483647).required(),
 });
 
-module.exports = { registerTimeBody: validator.body(querySchema) };
+module.exports = { registerTimeBody: validator.body(bodySchema) };
