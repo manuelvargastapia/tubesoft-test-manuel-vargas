270a8cc04d184943e883c2d17a1a830ed5ee972b
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-18 22:26:58 -0400

    adapt service to receive an object instead of primitive

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -3,7 +3,7 @@ const { Times } = require('../models');
 const createTime = async (registeredTime) => {
   const returnData = { newTime: null, error: null };
   try {
-    returnData.newTime = await Times.create({ registeredTime });
+    returnData.newTime = await Times.create(registeredTime);
   } catch (error) {
     returnData.error = error;
   }
