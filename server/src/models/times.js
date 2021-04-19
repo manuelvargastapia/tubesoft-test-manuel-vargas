39f4857b0acc5703965be631a388fec033f0a345
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-18 22:25:08 -0400

    modify model, add validations and modify toJSON

--- server/src/models/times.js
+++ server/src/models/times.js
@@ -3,9 +3,14 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Times extends Model {
     // Override `toJSON()` to hide `id` field, as we don't need it
-    // nor want to expose it
+    // nor want to expose it. Also, only return existent values
     toJSON() {
-      return { ...this.get(), id: undefined };
+      return {
+        ...this.get(),
+        id: undefined,
+        milliseconds: this.milliseconds != null ? this.milliseconds : undefined,
+        seconds: this.seconds != null ? this.seconds : undefined,
+      };
     }
   }
 
@@ -16,14 +21,36 @@ module.exports = (sequelize, DataTypes) => {
         defaultValue: DataTypes.UUIDV4,
         allowNull: false,
       },
-      registeredTime: {
+      milliseconds: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        validate: {
+          min: 0,
+          max: 2147483647,
+        },
+      },
+      seconds: {
+        type: DataTypes.INTEGER,
+        validate: {
+          min: 0,
+          max: 2147483647,
+        },
       },
     },
     {
       sequelize,
       modelName: 'Times',
+      validate: {
+        // Custom validator to ensure throwing an error if, even after
+        // previous validations, still some invalid data are tried to
+        // be stored
+        onlyOneOfTheFields() {
+          if (this.milliseconds && this.seconds) {
+            throw new Error(
+              "milliseconds and seconds can't be present simultaneously."
+            );
+          }
+        },
+      },
     }
   );
 
