07811db8774dc4bdd4572e3361233a111040d1ef
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-21 17:41:20 -0400

    test drive App.js content

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
+import StopWatch from './components/StopWatch/StopWatch';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Don't waste time!</p>
+        <p>Start, pause and then finish your stopwatch to record your time.</p>
+        <StopWatch />
       </header>
     </div>
   );
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders header messages', () => {
+    render(<App />);
+
+    expect(screen.getByText("Don't waste time!")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        'Start, pause and then finish your stopwatch to record your time.'
+      )
+    ).toBeInTheDocument();
+  });
+
+  it('renders Stopwatch', () => {
+    render(<App />);
+    expect(screen.getByTestId('stop-watch')).toBeInTheDocument();
+  });
 });
