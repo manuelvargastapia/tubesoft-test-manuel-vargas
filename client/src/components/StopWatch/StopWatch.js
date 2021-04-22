969eb093473faddfbef90b5df8352ea908816761
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-22 13:22:57 -0400

    add hooks to control timer, pass status and handlers to Buttons and time to Timers

--- client/src/components/StopWatch/StopWatch.js
+++ client/src/components/StopWatch/StopWatch.js
@@ -1,10 +1,61 @@
+import { useEffect, useState } from 'react';
 import Buttons from '../Buttons/Buttons';
+import Timer from '../Timer/Timer';
 
 function StopWatch() {
+  const [status, setStatus] = useState({
+    isRunning: false,
+    isPaused: false,
+    isStopped: true,
+  });
+  const [time, setTime] = useState(0);
+
+  const handleStart = () => {
+    setStatus({ isRunning: true, isPaused: false, isStopped: false });
+  };
+  const handlePause = () => {
+    setStatus({ isRunning: false, isPaused: true, isStopped: false });
+  };
+  const handleResume = () => {
+    setStatus({ isRunning: true, isPaused: false, isStopped: false });
+  };
+  const handleFinish = () => {
+    setStatus({ isRunning: false, isPaused: false, isStopped: true });
+  };
+
+  useEffect(() => {
+    let interval = null;
+
+    if (status.isRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    }
+
+    if (status.isPaused) {
+      clearInterval(interval);
+    }
+
+    if (status.isStopped) {
+      clearInterval(interval);
+      setTime(0);
+    }
+
+    // Clean up the interval after each render to prevent setTime()
+    // being called even after pausing or reseting the timer
+    // return () => clearInterval(interval);
+  }, [status]);
+
   return (
-    <div data-testid="stop-watch">
-      <p>00:00:000</p>
-      <Buttons />
+    <div data-testid="stopwatch">
+      <Timer time={time} />
+      <Buttons
+        status={status}
+        onStart={handleStart}
+        onPause={handlePause}
+        onResume={handleResume}
+        onFinish={handleFinish}
+      />
     </div>
   );
 }
