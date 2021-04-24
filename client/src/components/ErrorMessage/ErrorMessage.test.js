e8025080177e17da7b22dcd6f136a9eb92ecb66f
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-24 19:36:08 -0400

    test-drive new ErrorMessage component

---
+++ client/src/components/ErrorMessage/ErrorMessage.test.js
@@ -0,0 +1,16 @@
+import { render, screen, configure } from '@testing-library/react';
+import ErrorMessage from './ErrorMessage';
+
+describe('ErrorMessage', () => {
+  beforeEach(() => {
+    configure({ throwSuggestions: true });
+  });
+
+  it('renders error message', () => {
+    const testMessage = 'An error has ocurred while saving your time.';
+
+    render(<ErrorMessage message={testMessage} />);
+
+    expect(screen.getByText(testMessage)).toBeInTheDocument();
+  });
+});
