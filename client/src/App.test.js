62fde7087b8587c93e9e6f3fb2bf2d401246c2c5
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-24 19:31:53 -0400

    move test from App to Stopwatch

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,18 +1,9 @@
-import {
-  render,
-  screen,
-  configure,
-  fireEvent,
-  act,
-} from '@testing-library/react';
+import { render, screen, configure } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
   beforeEach(() => {
     configure({ throwSuggestions: true });
-
-    // Mock and reset setInterval() between tests
-    jest.useFakeTimers();
   });
 
   it('renders initial setup', () => {
@@ -25,91 +16,5 @@ describe('App', () => {
       )
     ).toBeInTheDocument();
     expect(screen.getByTestId('stopwatch')).toBeInTheDocument();
-  });
-
-  it('starts the timer when clicking Start button', () => {
-    render(<App />);
-
-    expect(setInterval).not.toHaveBeenCalled();
-    fireEvent.click(screen.getByRole('button', { name: /start/i }));
-    expect(setInterval).toHaveBeenCalledTimes(1);
-
-    // Forward setInterval 10 seconds.
-    // act() wrapper is required because we're updating the component's state
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
-  });
-
-  it('resets the timer when clicking Finish button', () => {
-    render(<App />);
-
-    expect(setInterval).not.toHaveBeenCalled();
-    fireEvent.click(screen.getByRole('button', { name: /start/i }));
-    expect(setInterval).toHaveBeenCalledTimes(1);
-
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
-    fireEvent.click(screen.getByRole('button', { name: /finish/i }));
-    expect(screen.getByTestId('timer')).toHaveTextContent('00:00:00');
-
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    expect(screen.getByTestId('timer')).toHaveTextContent('00:00:00');
-  });
-
-  it('stops the timer at the current value when clicking Pause button', () => {
-    render(<App />);
-
-    expect(setInterval).not.toHaveBeenCalled();
-    fireEvent.click(screen.getByRole('button', { name: /start/i }));
-    expect(setInterval).toHaveBeenCalledTimes(1);
-
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
-    fireEvent.click(screen.getByRole('button', { name: /pause/i }));
-    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
-
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
-  });
-
-  it('starts the timer when clicking Resume button', () => {
-    render(<App />);
-
-    expect(setInterval).not.toHaveBeenCalled();
-    fireEvent.click(screen.getByRole('button', { name: /start/i }));
-
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    fireEvent.click(screen.getByRole('button', { name: /pause/i }));
-
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
-    fireEvent.click(screen.getByRole('button', { name: /resume/i }));
-
-    act(() => {
-      jest.advanceTimersByTime(10000);
-    });
-
-    expect(screen.getByTestId('timer')).toHaveTextContent('00:20:00');
   });
 });
--- client/src/components/StopWatch/StopWatch.test.js
+++ client/src/components/StopWatch/StopWatch.test.js
@@ -1,11 +1,18 @@
-import { render, screen, configure } from '@testing-library/react';
+import {
+  render,
+  screen,
+  configure,
+  fireEvent,
+  act,
+} from '@testing-library/react';
 import StopWatch from './StopWatch';
 
 describe('Stopwatch', () => {
   beforeEach(() => {
-    configure({
-      throwSuggestions: true,
-    });
+    configure({ throwSuggestions: true });
+
+    // Mock and reset setInterval() between tests
+    jest.useFakeTimers();
   });
 
   it('renders initial setup', () => {
@@ -13,5 +20,91 @@ describe('Stopwatch', () => {
 
     expect(screen.getByTestId('timer')).toHaveTextContent('00:00:00');
     expect(screen.getByTestId('buttons')).toBeInTheDocument();
+  });
+
+  it('starts the timer when clicking Start button', () => {
+    render(<StopWatch />);
+
+    expect(setInterval).not.toHaveBeenCalled();
+    fireEvent.click(screen.getByRole('button', { name: /start/i }));
+    expect(setInterval).toHaveBeenCalledTimes(1);
+
+    // Forward setInterval 10 seconds.
+    // act() wrapper is required because we're updating the component's state
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
+  });
+
+  it('resets the timer when clicking Finish button', () => {
+    render(<StopWatch />);
+
+    expect(setInterval).not.toHaveBeenCalled();
+    fireEvent.click(screen.getByRole('button', { name: /start/i }));
+    expect(setInterval).toHaveBeenCalledTimes(1);
+
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
+    fireEvent.click(screen.getByRole('button', { name: /finish/i }));
+    expect(screen.getByTestId('timer')).toHaveTextContent('00:00:00');
+
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    expect(screen.getByTestId('timer')).toHaveTextContent('00:00:00');
+  });
+
+  it('stops the timer at the current value when clicking Pause button', () => {
+    render(<StopWatch />);
+
+    expect(setInterval).not.toHaveBeenCalled();
+    fireEvent.click(screen.getByRole('button', { name: /start/i }));
+    expect(setInterval).toHaveBeenCalledTimes(1);
+
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
+    fireEvent.click(screen.getByRole('button', { name: /pause/i }));
+    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
+
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
+  });
+
+  it('starts the timer when clicking Resume button', () => {
+    render(<StopWatch />);
+
+    expect(setInterval).not.toHaveBeenCalled();
+    fireEvent.click(screen.getByRole('button', { name: /start/i }));
+
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    fireEvent.click(screen.getByRole('button', { name: /pause/i }));
+
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    expect(screen.getByTestId('timer')).toHaveTextContent('00:10:00');
+    fireEvent.click(screen.getByRole('button', { name: /resume/i }));
+
+    act(() => {
+      jest.advanceTimersByTime(10000);
+    });
+
+    expect(screen.getByTestId('timer')).toHaveTextContent('00:20:00');
   });
 });
