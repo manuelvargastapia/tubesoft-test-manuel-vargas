7cb948f1d97c61ab223917b8e8991a63559873af
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-25 00:36:06 -0400

    handle errors and succesful records saving in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
-import './App.css';
 import StopWatch from './components/StopWatch/StopWatch';
+import LastRecord from './components/LastRecord/LastRecord';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import CriticalErrorPage from './components/CriticalErrorPage/CriticalErrorPage';
+import useSaveRecord from './useSaveRecord';
+import './App.css';
 
 function App() {
+  const [
+    lastRecord,
+    temporalError,
+    criticalError,
+    onStopwatchFinished,
+  ] = useSaveRecord();
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Don't waste time!</p>
-        <p>Start, pause and then finish your stopwatch to record your time.</p>
-        <StopWatch />
+        {criticalError ? (
+          <CriticalErrorPage details={criticalError} />
+        ) : (
+          <div>
+            <p>Don't waste time!</p>
+            <p>
+              Start, pause and then finish your stopwatch to record your time.
+            </p>
+            <StopWatch onStopwatchFinished={onStopwatchFinished} />
+            {lastRecord && <LastRecord time={lastRecord} />}
+            {temporalError && <ErrorMessage message={temporalError} />}
+          </div>
+        )}
       </header>
     </div>
   );
